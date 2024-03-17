06361e2f028d48f9fe159877a8ea6975a68eb548
Girish <girish624@gmail.com>
2024-03-17 21:05:56 +0530

    first

---
+++ a.sh
@@ -0,0 +1,40 @@
+echo line1 >> file1
+echo line1 >> file2
+echo line1 >> file3
+echo line1 >> file4
+echo line1 >> file5
+git add .
+git commit . -m "first"
+
+echo line2 >> file1
+echo line2 >> file2
+echo line2 >> file3
+echo line2 >> file4
+echo line2 >> file5
+git add .
+git commit . -m "second"
+
+echo line3 >> file1
+echo line3 >> file2
+echo line3 >> file3
+echo line3 >> file4
+echo line3 >> file5
+git add .
+git commit . -m "third"
+
+echo line4 >> file1
+echo line4 >> file2
+echo line4 >> file3
+echo line4 >> file4
+echo line4 >> file5
+git add .
+git commit . -m "fourth"
+
+echo line5 >> file1
+echo line5 >> file2
+echo line5 >> file3
+echo line5 >> file4
+echo line5 >> file5
+git add .
+git commit . -m "fifth"
+
